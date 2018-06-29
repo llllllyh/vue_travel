ee3e69200f0cf20d77064640b6f66316a414a444
刘远辉 <779937374@qq.com>
2018-06-29 10:44:34 +0800

    项目初始化

---
+++ myproject/src/pages/home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>Hello</div>
+    <router-link to="/list">列表</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style>
+</style>
